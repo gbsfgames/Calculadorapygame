7d8d043da02989b135b7e42c0b1bd452d9236b48
benyair <gbsfgames@gmail.com>
2019-06-21 18:16:11 -0500

    Actualizacion de Efecto de botones y optimizacion de codigo

--- Calculadora.py
+++ Calculadora.py
@@ -22,7 +22,8 @@ fil4 = fil1 + 135
 
 ancho = 200
 alto = 270
-
+tecla = None
+tecla_nom = None
 #--------------------------funciones
 # Clase de tecla
 class tecla():
@@ -33,6 +34,25 @@ class tecla():
         return ((col,fill))
     def imagen(tecla = pygame.image.load('calculadora/null.png')):
         return (tecla)
+    
+def presionada(tecla, pantalla,evento,posicion):
+    '''tecla tiene que ser surface --- pantalla: es la pantalla principal'''
+    pantalla.blit(tecla,posicion)
+    if evento.type ==MOUSEBUTTONDOWN:
+        if evento.button ==1:
+            print("tecla :",dir(tecla))
+            print("tecla :",tecla.__gt__)
+            tecla.set_alpha(70)
+            tecla.fill((0,0,0))
+            pantalla.blit(tecla,posicion)
+    return 0
+
+def ponernum(ltecla_nom):
+    ''''''
+    expresion = []
+    letra_arial = pygame.font.SysFont('arial',14)
+    pantalla.blit(letra_arial.render())
+    pass
     
 # Funcion principal
 def main():
@@ -91,56 +111,63 @@ def main():
         if(mouse_pos[0] >=col1 and mouse_pos[1] >= fil1 and mouse_pos[0]<=(col1 + tam_tec[0]) and mouse_pos[1]<=(fil1 + tam_tec[1])):
             arriba = True
             siete7_a = pygame.transform.smoothscale(siete7,(tam_tec[0]+6,tam_tec[1]+6))
-            pantalla.blit(siete7_a,(col1-3,fil1-3))
-            if evento.type == MOUSEBUTTONDOWN:
-                if evento.button ==1:
-                    pass
+            siete7_p = (col1-3,fil1-3)
+            presionada(siete7_a, pantalla, evento,siete7_p) 
                                     
         #Tecla 8
         if(mouse_pos[0] >=col2 and mouse_pos[1] >= fil1 and mouse_pos[0]<=(col2 + tam_tec[0]) and mouse_pos[1]<=(fil1 + tam_tec[1])):
             arriba = True
             ocho8_a = pygame.transform.smoothscale(ocho8,(tam_tec[0]+6,tam_tec[1]+6))
-            pantalla.blit(ocho8_a,(col2-3,fil1-3))
+            ocho8_p = (col2-3,fil1-3)
+            presionada(ocho8_a, pantalla, evento,ocho8_p) 
         #Tecla 9
         if(mouse_pos[0] >=col3 and mouse_pos[1] >= fil1 and mouse_pos[0]<=(col3 + tam_tec[0]) and mouse_pos[1]<=(fil1 + tam_tec[1])):
             arriba = True
             nueve9_a = pygame.transform.smoothscale(nueve9,(tam_tec[0]+6,tam_tec[1]+6))
-            pantalla.blit(nueve9_a,(col3-3,fil1-3))
+            nueve9_p = (col3-3,fil1-3)
+            presionada(nueve9_a, pantalla, evento,nueve9_p) 
         #Tecla 4
         if(mouse_pos[0] >=col1 and mouse_pos[1] >= fil2 and mouse_pos[0]<=(col1 + tam_tec[0]) and mouse_pos[1]<=(fil2 + tam_tec[1])):
             arriba = True
             cuatro4_a = pygame.transform.smoothscale(cuatro4,(tam_tec[0]+6,tam_tec[1]+6))
-            pantalla.blit(cuatro4_a,(col1-3,fil2-3))
+            cuatro4_p = (col1-3,fil2-3)
+            presionada(cuatro4_a, pantalla, evento,cuatro4_p)
         #Tecla 5
         if(mouse_pos[0] >=col2 and mouse_pos[1] >= fil2 and mouse_pos[0]<=(col2 + tam_tec[0]) and mouse_pos[1]<=(fil2 + tam_tec[1])):
             arriba = True
             cinco5_a = pygame.transform.smoothscale(cinco5,(tam_tec[0]+6,tam_tec[1]+6))
-            pantalla.blit(cinco5_a,(col2-3,fil2-3))
+            cinco5_p = (col2-3,fil2-3)
+            presionada(cinco5_a, pantalla, evento,cinco5_p)
         #Tecla 6
         if(mouse_pos[0] >=col3 and mouse_pos[1] >= fil2 and mouse_pos[0]<=(col3 + tam_tec[0]) and mouse_pos[1]<=(fil2 + tam_tec[1])):
             arriba = True
             seis6_a = pygame.transform.smoothscale(seis6,(tam_tec[0]+6,tam_tec[1]+6))
-            pantalla.blit(seis6_a,(col3-3,fil2-3))
+            seis6_p = (col3-3,fil2-3)
+            presionada(seis6_a, pantalla, evento,seis6_p)
         #Tecla 1
         if(mouse_pos[0] >=col1 and mouse_pos[1] >= fil3 and mouse_pos[0]<=(col1 + tam_tec[0]) and mouse_pos[1]<=(fil3 + tam_tec[1])):
             arriba = True
             uno1_a = pygame.transform.smoothscale(uno1,(tam_tec[0]+6,tam_tec[1]+6))
-            pantalla.blit(uno1_a,(col1-3,fil3-3))
+            uno1_p = (col1-3,fil3-3)
+            presionada(uno1_a, pantalla, evento,uno1_p)
         #Tecla 2
         if(mouse_pos[0] >=col2 and mouse_pos[1] >= fil3 and mouse_pos[0]<=(col2 + tam_tec[0]) and mouse_pos[1]<=(fil3 + tam_tec[1])):
             arriba = True
             dos2_a = pygame.transform.smoothscale(dos2,(tam_tec[0]+6,tam_tec[1]+6))
-            pantalla.blit(dos2_a,(col2-3,fil3-3))
+            dos2_p = (col2-3,fil3-3)
+            presionada(dos2_a, pantalla, evento,dos2_p)
         #Tecla 3
         if(mouse_pos[0] >=col3 and mouse_pos[1] >= fil3 and mouse_pos[0]<=(col3 + tam_tec[0]) and mouse_pos[1]<=(fil3 + tam_tec[1])):
             arriba = True
             tre3_a = pygame.transform.smoothscale(tres3,(tam_tec[0]+6,tam_tec[1]+6))
-            pantalla.blit(tre3_a,(col3-3,fil3-3))
+            tre3_p = (col3-3,fil3-3)
+            presionada(tre3_a, pantalla, evento,tre3_p)
         #Tecla 0
         if(mouse_pos[0] >=col2 and mouse_pos[1] >= fil4 and mouse_pos[0]<=(col2 + tam_tec[0]) and mouse_pos[1]<=(fil4 + tam_tec[1])):
             arriba = True
             cero_a = pygame.transform.smoothscale(cero,(tam_tec[0]+6,tam_tec[1]+6))
-            pantalla.blit(cero_a,(col2-3,fil4-3))
+            cero_p = (col2-3,fil4-3)
+            presionada(cero_a, pantalla, evento,cero_p)
         '''Fin fragmento sobre de'''   
         pygame.display.update()
 
